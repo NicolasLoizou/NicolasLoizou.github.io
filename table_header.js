31176b878c33ac8d1442d0d2cb0ed4a1d75960bc
NicolasLoizou <nicolasloizou1@gmail.com>
2025-11-12 20:18:40 -0500

    update

--- table_header.js
+++ table_header.js
@@ -21,7 +21,7 @@ document.write("<a href=\"https:\/\/scholar.google.com\/citations?user=mvDmzAQAA
 document.write("<a href=\"https:\/\/dblp.org\/pid\/173\/4958.html\"><img src=\"Images/dblp.png\" alt=\"dblp\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
 document.write("<a href=\"https:\/\/www.semanticscholar.org\/author\/N.-Loizou\/1941365\"><img src=\"Images/semantic.png\" alt=\"Semantic Scholar\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
 document.write("<a href=\"https:\/\/www.linkedin.com\/in\/nicolas-loizou-6a2561b0\/\"> <img src=\"Images\/linkedin.png\" alt=\"Linked In\" width=\"25\" border=\"0\" height=\"25\"><\/a> &nbsp;");
-document.write("<a href=\"https:\/\/twitter.com\/NicLoizou\/\"> <img src=\"Images\/twitter.png\" alt=\"Twitter\"  border=\"0\" height=\"25\"><\/a><br>");
+document.write("<a href=\"https:\/\/twitter.com\/NicLoizou\/\"> <img src=\"Images\/xlogo2.png\" alt=\"Twitter\"  border=\"0\" height=\"25\"><\/a><br>");
 document.write("<\/td>");
 document.write("<\/tr>");
 document.write("<\/tbody>");
